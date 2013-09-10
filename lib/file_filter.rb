b7cf768362ffcb03c55136599ccf71ee121275db
akicho8 <akicho8@gmail.com>
2013-09-10 11:18:16 +0900

    pptx を除外

--- lib/file_filter.rb
+++ lib/file_filter.rb
@@ -31,7 +31,7 @@ module FileFilter
     list = [
       # 拡張子
       /\.(cache|schemas|old|bak|orig|rej|a|o|Z|elc|ln|rbc|\.del-.*)\z/,
-      /\.(fla|flv|avi||ttf|mp3|mov|mp4|zip|lzh|mpg|jpg|bmp|wav|xm|mid|gif|tar|gz|png|db|swf|svg|diff|xls|ppt|ico|pid|tmp)\z/i,
+      /\.(pptx|fla|flv|avi||ttf|mp3|mov|mp4|zip|lzh|mpg|jpg|bmp|wav|xm|mid|gif|tar|gz|png|db|swf|svg|diff|xls|ppt|ico|pid|tmp)\z/i,
       # 単語
       /\b(RDEFSX|RDEFS|RCSLOG|RCS|SCCS|TAGS|CHANGELOG|\.make\.sate|\.nse_depinfo|CVS|cvslog|svn|git|log|DS_Store)\b/i,
       /\b(cache|password_dic|coverage|public\/assets)\b/,
