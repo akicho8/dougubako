1cdc63d426869db537d7c4fdf90393197e8393e7
akicho8 <akicho8@gmail.com>
2021-09-14 10:10:26 +0900

    [feat] /public/ 除外

--- lib/file_ignore.rb
+++ lib/file_ignore.rb
@@ -87,6 +87,7 @@ module FileIgnore
       /\b(cache|password_dic|coverage|public\/assets)\b/,
       /\b(doc\/app|coverage|pkg|ruby_sess|yardoc|rdoc)\b/,
       /tmp.*meta_request.*json\z/, # rails tmp/data/meta_request/91f33f2a0bbf97d42fc1b1c95915fc91.json
+      %{/public/},
       /\b(dist|cache|password_dic|coverage|public\/assets)\b/,
       /\.bundle\b/,
       # ゴミ
